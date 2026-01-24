24a16efb2187fcd457848fb269891306805e9440
veerababu <vicky8337284@gmail.com>
2026-01-24 11:16:47 +0530

    Fix: Final connection between Vercel and Render

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const serviceRoutes=require("./routes/serviceRoutes.js")
 const app = express();
 
 const corsOptions = {
-  origin: ["https://service-booking-website-beta.vercel.app", "https://service-booking-website-5.onrender.com/"],
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
